23fe6f68c16fd852c7dcbc26b1d791d638331600
giovi <giovadesio26@gmail.com>
2024-02-13 00:03:50 +0100

    set up eslint (find a way to fix all the bunch of errors)

--- synchub/tailwind.config.js
+++ synchub/tailwind.config.js
@@ -3,20 +3,20 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
-    ],
+	content: [
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		'./storage/framework/views/*.php',
+		'./resources/views/**/*.blade.php',
+		'./resources/js/**/*.jsx',
+	],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
 
-    plugins: [forms],
+	plugins: [forms],
 };
